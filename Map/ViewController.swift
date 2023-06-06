dc9fbbbd24b7ecbd40608f1562e8e63ae8c36569
minwoo129 <key0129mw@naver.com>
2023-06-06 14:02:51 +0900

    지도 보여주기

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -8,15 +8,23 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var myMap: MKMapView!
-    
     @IBOutlet weak var lblLocationInfo1: UILabel!
-    
     @IBOutlet weak var lblLocationInfo2: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        lblLocationInfo1.text = ""
+        lblLocationInfo2.text = ""
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        myMap.showsUserLocation = true
         // Do any additional setup after loading the view.
     }
 
