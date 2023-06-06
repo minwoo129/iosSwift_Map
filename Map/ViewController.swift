6c950bed5b827019ee9c91018f74cbce39e28869
minwoo129 <key0129mw@naver.com>
2023-06-06 16:17:24 +0900

    위도, 경도로 원하는 핀 설치

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -28,16 +28,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Do any additional setup after loading the view.
     }
     
-    func goLoaction(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span :Double) {
+    func goLoaction(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span :Double) -> CLLocationCoordinate2D {
         let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
         let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span);
         let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
         myMap.setRegion(pRegion, animated: true)
+        return pLocation
+    }
+    
+    func setAnnotation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span :Double, title strTitle: String, subTitle strSubtitle:String) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLoaction(latitudeValue: latitudeValue, longitudeValue: longitudeValue, delta: span);
+        annotation.title = strTitle
+        annotation.subtitle = strSubtitle
+        myMap.addAnnotation(annotation)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
         let pLocation = locations.last
-        goLoaction(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        _ = goLoaction(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
         CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
             (placemarks, error) -> Void in
             let pm = placemarks!.first
@@ -54,6 +63,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
 
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+        if(sender.selectedSegmentIndex == 0) {
+            
+        }
+        else if(sender.selectedSegmentIndex == 1) {
+            setAnnotation(latitudeValue: 37.751853, longitudeValue: 128.87605740000004, delta: 1, title: "한국폴리택대학 강릉캠퍼스", subTitle: "강원도 강릉시 남산초교길 121")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "한국폴리택대학 강릉캠퍼스"
+        }
+        else if(sender.selectedSegmentIndex == 2) {
+            setAnnotation(latitudeValue: 37.556876, longitudeValue: 126.914066, delta: 0.1, title: "이지스퍼블리싱", subTitle: "서울시 마포구 잔다리로 109 이지스 빌딩")
+            self.lblLocationInfo1.text = "보고계신 위치"
+            self.lblLocationInfo2.text = "이지스퍼블리싱 출판사"
+        }
     }
     
 }
