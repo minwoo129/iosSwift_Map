e94cfcd980fa59fc61cfa7418efac1c769024e07
minwoo129 <key0129mw@naver.com>
2023-06-06 13:56:28 +0900

    지도뷰, 라벨, 세그먼트 셀랙터 아웃렛 변수, 액션함수 추가

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMap: MKMapView!
+    
+    @IBOutlet weak var lblLocationInfo1: UILabel!
+    
+    @IBOutlet weak var lblLocationInfo2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+    }
+    
 }
 
